4df367a7d9d80ce2e20ce47169bff677d894cd3b
Phillip-Juan van der Berg <phillip-juan@zailab.com>
2023-09-27 11:50:18 +0200

    add pedendency logs

--- app.js
+++ app.js
@@ -1,41 +1,49 @@
 console.log('App | Loading...');
 
-console.log('App | Loading third party dependencies');
-console.log('App | Loading third party dependencies | http-errors ');
-var createError = require('http-errors');
-console.log('App | Loading third party dependencies | http-errors loaded ');
-console.log('App | Loading third party dependencies | express ');
+console.log('App | Loading third party dependencies...');
 
-var express;
+let dependencies = {
+  createError: null,
+  express: null,
+  path: null,
+  cookieParser: null,
+  logger: null,
+  cors: null,
+  mongoose: null,
+  passport: null
+};
 
-try {
-  express = require('express');
-} catch(e) {
-  console.log(' ----------- an error occured loading express ', e);
+function load(variable, dependency) {
+  console.log('App | Loading dependencies | ' + dependency);
+  dependencies[variable] = require(dependency);
+  console.log('App | Loading third party dependencies | ' + dependency + ' loaded ');
 }
 
+// var createError = require('http-errors');
+load('createError', 'http-errors');
 
-console.log('App | Loading third party dependencies | express loaded ');
-console.log('App | Loading third party dependencies | path ');
-var path = require('path');
-console.log('App | Loading third party dependencies | path loaded ');
-console.log('App | Loading third party dependencies | cookie-parser ');
-var cookieParser = require('cookie-parser');
-console.log('App | Loading third party dependencies | cookie-parser loaded ');
-console.log('App | Loading third party dependencies | morgan ');
-var logger = require('morgan');
-console.log('App | Loading third party dependencies | morgan loaded ');
-console.log('App | Loading third party dependencies | cors ');
-const cors = require('cors');
-console.log('App | Loading third party dependencies | cors loaded ');
-console.log('App | Loading third party dependencies | mongoose ');
-var mongoose = require('mongoose');
-console.log('App | Loading third party dependencies | mongoose loaded ');
-console.log('App | Loading third party dependencies | passport ');
-const passport = require('passport');
-console.log('App | Loading third party dependencies | passport loaded ');
+// var express = require('express');
+load('express', 'express');
 
-console.log('App | Loading dependencies');
+// var path = require('path');
+load('path', 'path');
+
+// var cookieParser = require('cookie-parser');
+load('cookieParser', 'cookie-parser');
+
+// var logger = require('morgan');
+load('logger', 'morgan');
+
+// const cors = require('cors');
+load('cors', 'cors');
+
+// var mongoose = require('mongoose');
+load('mongoose', 'mongoose');
+
+// const passport = require('passport');
+load('passport', 'passport');
+
+console.log('App | Loading dependencies...');
 var indexRouter = require('./routes/index');
 var authRouter = require('./routes/auth');
 var envRouter = require('./routes/environments');
@@ -46,17 +54,17 @@ var sitesRouter = require('./routes/sites');
 
 console.log('App | Dependencies... Loaded');
 
-var app = express();
-app.use(express.urlencoded({ extended: false }));
-app.use(passport.initialize());
-app.use(cors());
+var app = dependencies.express();
+app.use(dependencies.express.urlencoded({ extended: false }));
+app.use(dependencies.passport.initialize());
+app.use(dependencies.cors());
 
 // const uri = "mongodb://127.0.0.1:27017/e2e-testing-projector"; // local db
 // const uri = "mongodb://192.168.1.6:27017/e2e-testing-projector"; // local db from docker container
 const uri = "mongodb://projectionrw:ABWturARBF98MPlcQ4Y=@192.168.3.36:27017/admin"; // dev db
 
 console.log('Connecting to mongo ...');
-mongoose.connect(
+dependencies.mongoose.connect(
   uri, {
     useNewUrlParser: true,
     useUnifiedTopology: true
@@ -69,14 +77,14 @@ mongoose.connect(
 });
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', dependencies.path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(dependencies.logger('dev'));
+app.use(dependencies.express.json());
+app.use(dependencies.express.urlencoded({ extended: false }));
+app.use(dependencies.cookieParser());
+app.use(dependencies.express.static(dependencies.path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
   // Allow all origins for demonstration purposes; consider restricting origins in production
@@ -97,7 +105,7 @@ app.use('/sites', sitesRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+  next(dependencies.createError(404));
 });
 
 // error handler
